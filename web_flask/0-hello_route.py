7dda5f696b24ad1ac52eab7102581f260a42ca9c
44caleb <calebadinfono2@gmail.com>
2023-10-21 13:57:29 +0000

    pycodestylecheck

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,30 @@
 
 
 from flask import Flask
-
+from markupsafe import escape
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def home():
     """home page of webapp"""
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """displays c follwed by text in the url"""
+    if "_" in text:
+        text = text.replace("_", " ")
+    return "C {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,15 +7,18 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home():
     """home page of webapp"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """displays HBNB"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def text(text):
@@ -23,6 +26,7 @@ def text(text):
     if "_" in text:
         text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
